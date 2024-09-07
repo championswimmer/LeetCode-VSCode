9d78b78979c3bb6f8faff0c85f09add16a5c60ca
Arnav Gupta <dev@championswimmer.in>
2024-09-07 21:24:10 +0100

    17: 7 min
    Signed-off-by: Arnav Gupta <dev@championswimmer.in>

---
+++ 17.letter-combinations-of-a-phone-number.js
@@ -0,0 +1,86 @@
+/*
+ * @lc app=leetcode id=17 lang=javascript
+ *
+ * [17] Letter Combinations of a Phone Number
+ *
+ * https://leetcode.com/problems/letter-combinations-of-a-phone-number/description/
+ *
+ * algorithms
+ * Medium (61.37%)
+ * Likes:    18791
+ * Dislikes: 1014
+ * Total Accepted:    2.2M
+ * Total Submissions: 3.5M
+ * Testcase Example:  '"23"'
+ *
+ * Given a string containing digits from 2-9 inclusive, return all possible
+ * letter combinations that the number could represent. Return the answer in
+ * any order.
+ * 
+ * A mapping of digits to letters (just like on the telephone buttons) is given
+ * below. Note that 1 does not map to any letters.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: digits = "23"
+ * Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: digits = ""
+ * Output: []
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: digits = "2"
+ * Output: ["a","b","c"]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 0 <= digits.length <= 4
+ * digits[i] is a digit in the range ['2', '9'].
+ * 
+ * 
+ */
+
+// @lc code=start
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function(digits) {
+    const digitMap = {
+        '2': ['a', 'b', 'c'],
+        '3': ['d', 'e', 'f'],
+        '4': ['g', 'h', 'i'],
+        '5': ['j', 'k', 'l'],
+        '6': ['m', 'n', 'o'],
+        '7': ['p', 'q', 'r', 's'],
+        '8': ['t', 'u', 'v'],
+        '9': ['w', 'x', 'y', 'z'],
+    };
+    const result = [];
+    if (digits.length === 0) return result;
+    const backtrack = (index, path) => {
+        if (index === digits.length) {
+            result.push(path);
+            return;
+        }
+        for (let letter of digitMap[digits[index]]) {
+            backtrack(index + 1, path + letter);
+        }
+    };
+    backtrack(0, '');
+    return result;
+};
+// @lc code=end
+
