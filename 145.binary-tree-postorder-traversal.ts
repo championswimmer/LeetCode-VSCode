896bd403df4a51eb9cd908c004770acc9afe0f1a
Arnav Gupta <dev@championswimmer.in>
2024-09-08 14:52:34 +0100

    post-order-traversal
    Signed-off-by: Arnav Gupta <dev@championswimmer.in>

---
+++ 145.binary-tree-postorder-traversal.ts
@@ -0,0 +1,100 @@
+/*
+ * @lc app=leetcode id=145 lang=typescript
+ *
+ * [145] Binary Tree Postorder Traversal
+ *
+ * https://leetcode.com/problems/binary-tree-postorder-traversal/description/
+ *
+ * algorithms
+ * Easy (72.01%)
+ * Likes:    7165
+ * Dislikes: 203
+ * Total Accepted:    1.4M
+ * Total Submissions: 1.9M
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * Given the root of a binary tree, return the postorder traversal of its
+ * nodes' values.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: root = [1,null,2,3]
+ * 
+ * Output: [3,2,1]
+ * 
+ * Explanation:
+ * 
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+ * 
+ * Output: [4,6,7,5,2,9,8,3,1]
+ * 
+ * Explanation:
+ * 
+ * 
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: root = []
+ * 
+ * Output: []
+ * 
+ * 
+ * Example 4:
+ * 
+ * 
+ * Input: root = [1]
+ * 
+ * Output: [1]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * The number of the nodes in the tree is in the range [0, 100].
+ * -100 <= Node.val <= 100
+ * 
+ * 
+ * 
+ * Follow up: Recursive solution is trivial, could you do it iteratively?
+ */
+
+// @lc code=start
+
+// class TreeNode {
+//   val: number
+//   left: TreeNode | null
+//   right: TreeNode | null
+//   constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+//     this.val = (val === undefined ? 0 : val)
+//     this.left = (left === undefined ? null : left)
+//     this.right = (right === undefined ? null : right)
+//   }
+// }
+
+
+function postorderTraversal(root: TreeNode | null): number[] {
+  const result: number[] = [];
+
+  function postorder(node: TreeNode | null) {
+    if (!node) return;
+    postorder(node.left);
+    postorder(node.right);
+    result.push(node.val);
+  }
+
+  postorder(root);
+  return result;
+};
+// @lc code=end
+
